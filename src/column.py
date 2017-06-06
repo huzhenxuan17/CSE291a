8f5339a6890c5942222cd2d89bed8f5ee354883a
zhenxuan hu <huzhenxuan171@gmail.com>
2017-06-06 15:35:50 -0700

    add merge function

--- src/column.py
+++ src/column.py
@@ -13,7 +13,7 @@ class Column:
         self.mt = mem_table.MEMTable()
 
     def add(self, key, value):
-        value = value.replace(DELETE_FLAG, "/"+DELETE_FLAG)
+        value = value.replace(DELETE_FLAG, "/" + DELETE_FLAG)
         persistence_flag = self.mt.add(key, value)
         if persistence_flag:
             self.persistence()
@@ -60,39 +60,89 @@ class Column:
         :return:
         """
         ss_table_dict = self.mt.mem_table
-        file_name = "data/" + self.col_name + '/small' + str(len(self.smallFile)) 
-        st = ss_table.SSTable(ss_table_dict, file_name) # inside : update bloom filter
+        file_name = "data/" + self.col_name + '/small' + str(len(self.smallFile))
+        st = ss_table.SSTable(ss_table_dict, file_name)  # inside : update bloom filter
         self.smallFile.append(st)
         self.mt.clear()
-        
         # merge smallFile
         if len(self.smallFile) >= MERGE_THRESHOLD:
             self.merge_sstable()
 
     def merge_sstable(self):
         sstable_list = []
-        for sstable in reversed(self.smallFile):
+        for sstable in self.smallFile:
             sstable_list.append(sstable.to_list())
         new_sstable_list = Column.merge_list(sstable_list)
-        file_name = "data/" + self.col_name + '/big' + str(len(self.bigFile)) 
+        file_name = "data/" + self.col_name + '/big' + str(len(self.bigFile))
         st = ss_table.SSTable(new_sstable_list, file_name)
         self.bigFile.append(st)
+        self.smallFile = []
 
     @staticmethod
     def merge_list(sstable_raw):
         length = len(sstable_raw)
         if length == 1:
             return sstable_raw[0]
-        list1 = Column.merge_list(ss_table[:length//2])
-        list2= Column.merge_list(ss_table[length//2:])
-        #TODO merge
+        list1 = Column.merge_list(ss_table[:length // 2])
+        list2 = Column.merge_list(ss_table[length // 2:])
+        result = []
+        len1 = len(list1)
+        len2 = len(list2)
+        i1 = 0
+        i2 = 0
+        while i1 < len1 and i2 < len2:
+            if list1[i1][0] == list2[i2][0]:
+                # list2 is newer than list1
+                result.append(list2[i2])
+                i1 += 1
+                i2 += 1
+            elif list1[i1][0] < list2[i2][0]:
+                result.append(list1[i1])
+                i1 += 1
+            else:
+                result.append(list2[i2])
+                i2 += 1
+        if i1 < len1:
+            result.extend(list1[i1:])
+        elif i2 < len2:
+            result.extend(list2[i2:])
+        return result
+
 
 if __name__ == "__main__":
-    cc = Column()
-    cc.add("ccc ", "haha")
-    cc.add("aaa", "you are my sunshine")
-    cc.add("bb", "my only sunshine")
-    cc.add("a", "LoL")
-    cc.persistence()
-    ad = cc.smallFile[0].to_list()
-    print ad
+    # cc = Column("test")
+    # cc.add("ccc ", "haha")
+    # cc.add("aaa", "you are my sunshine")
+    # cc.add("bb", "my only sunshine")
+    # cc.add("a", "LoL")
+    # cc.persistence()
+    # ad = cc.smallFile[0].to_list()
+    # print ad
+
+    """
+    test merge
+    """
+    # list1 = [("a", "123"), ("b", "123"), ("d", "000"),("f","222")]
+    # list2 = [("a", "223"), ("bb", "444"), ("c", "555"), ("d", "666")]
+    # result = []
+    # len1 = len(list1)
+    # len2 = len(list2)
+    # i1 = 0
+    # i2 = 0
+    # while i1 < len1 and i2 < len2:
+    #     if list1[i1][0] == list2[i2][0]:
+    #         # list2 is newer than list1
+    #         result.append(list2[i2])
+    #         i1 += 1
+    #         i2 += 1
+    #     elif list1[i1][0] < list2[i2][0]:
+    #         result.append(list1[i1])
+    #         i1 += 1
+    #     else:
+    #         result.append(list2[i2])
+    #         i2 += 1
+    # if i1 < len1:
+    #     result.extend(list1[i1:])
+    # elif i2 < len2:
+    #     result.extend(list2[i2:])
+    # print result
